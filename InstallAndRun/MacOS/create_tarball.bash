898e62f959b944779ecc68161e97b0de8bb40193
E M Thornber <enchanted.systems@btinternet.com>
2026-07-01 15:11:20 +0100

    Inital commit on branch

---
+++ InstallAndRun/MacOS/create_tarball.bash
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+################################################################################
+#   Script to create a tarball of the MMC-SERVER application for distribution.
+#  
+#   1 July 2026 - E M Thornber
+#   Created
+#
+################################################################################
+
+APP_NAME="MMC-SERVER"
+EXCLUDE_FILE="MMC-SERVER/InstallAndRun/MacOS/excluded_files.txt"
+TAR=/usr/bin/tar
+VERSION=`npm pkg get version | tr -d '"'`
+GZIP=/usr/bin/gzip
+
+( cd .. ; \
+  ${TAR} -cvf - --exclude-vcs --exclude-from ${EXCLUDE_FILE} MMC-SERVER/* ) | \
+  ${GZIP} -9 > InstallAndRun/MacOS/${APP_NAME}-src-${VERSION}.tar.gz
---
+++ InstallAndRun/MacOS/node_index.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+cd "$(dirname "$0")/../Resources"
+exec /usr/local/bin/node main.js
